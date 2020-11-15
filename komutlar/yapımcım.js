be124f17431849fe79b67bf55b47024c308c5a9d
talhaerbil <74492344+talhaerbil@users.noreply.github.com>
2020-11-15 14:27:47 +0200

    Create yapımcım.js

---
+++ komutlar/yapımcım.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+exports.run = (client, message) => {
+  if (message.channel.type !== 'dm') {
+    const ozelmesajkontrol = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .setColor('RANDOM')
+    .setTimestamp()
+    .addField('**Yapımcım: **', '★𝖙𝖆𝖑𝖍𝖆𝖄𝕿★#9999')
+    message.channel.sendEmbed(ozelmesajkontrol) }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['yapımcım'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'yapımcım',
+  description: 'Yapimcimi Gosterir.',
+  usage: 'yapimcim'
+};
